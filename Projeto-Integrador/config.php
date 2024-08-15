e288ff28a1abff300176bb48c7af233f25619f63
Johnny Souza <127693257+JEMS2001@users.noreply.github.com>
2024-08-15 18:33:32 -0300

    Update config.php

--- Projeto-Integrador/config.php
+++ Projeto-Integrador/config.php
@@ -1,8 +1,8 @@
 <?php
 // Dados de conexão com o banco de dados
 $servername = "mysql";  // Nome do servidor MySQL (geralmente 'localhost')
-$username = "root";  // Nome de usuário do MySQL
-$password = "root_password";    // Senha do MySQL
+$username = "jfhk";  // Nome de usuário do MySQL
+$password = "jfhk123";    // Senha do MySQL
 $dbname = "projeto";  // Nome do banco de dados que você criou
 
 // Cria a conexão
